4679af079971cc9ba6d963cb4ca2d90aef285740
Shivam H Patel <phenomenalshivam2@gmail.com>
2025-09-24 13:01:11 +0530

    Small Modal Wrapper Change for medium

--- frontend/src/pages/Common/AgencyInformation/component/DeleteMeetingMinutes.tsx
+++ frontend/src/pages/Common/AgencyInformation/component/DeleteMeetingMinutes.tsx
@@ -53,7 +53,7 @@ const DeleteMeetingMinutes: React.FC<DeleteMeetingProps> = ({
       <ModalWrapper
         show={showModal}
         title="Delete Meeting Minutes"
-        size="md"
+        size="lg"
         headerClassName="text-xl p-0 pb-20 text-street-dark"
         className="p-20 p-sm-24 p-md-32 gap-16"
         bodyClassName="p-0 d-flex flex-column gap-16"
