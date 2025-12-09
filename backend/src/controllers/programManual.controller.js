c787d41f65a806cb2a5eba62dc69d645b8a00c03
Shivam H Patel <phenomenalshivam2@gmail.com>
2025-12-09 16:47:23 +0530

    Added role Dropdown in Signup

--- backend/src/controllers/programManual.controller.js
+++ backend/src/controllers/programManual.controller.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import { getPdfPageCount } from "../helper/pdfpagecount.js";
-import { activeTicketUsers, io } from "../index.js";
-import Notification from "../model/notification.js";
+import {  io } from "../index.js";
+
 import ProgramManual from "../model/programManuals.js";
-import User from "../model/user.js";
+
 import { ApiError } from "../utills/ApiError.js";
 import { ApiResponse } from "../utills/ApiResponse.js";
 import { asyncHandler } from "../utills/AsyncHandler.js";
