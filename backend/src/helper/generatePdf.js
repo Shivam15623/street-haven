a8af8574d80f9eaebb0dd8a59c07345e9472afcc
Shivam H Patel <phenomenalshivam2@gmail.com>
2026-01-01 10:27:32 +0530

    chrome added

--- backend/src/helper/generatePdf.js
+++ backend/src/helper/generatePdf.js
@@ -3,16 +3,26 @@ import puppeteer from "puppeteer";
 export const generatePdf = async (html) => {
   const browser = await puppeteer.launch({
     headless: "new",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--single-process",
+    ],
   });
 
   const page = await browser.newPage();
+
   await page.setContent(html, { waitUntil: "networkidle0" });
 
   const pdfBuffer = await page.pdf({
     format: "A4",
     printBackground: true,
+    preferCSSPageSize: true,
   });
 
+  await page.close();
   await browser.close();
+
   return pdfBuffer;
 };
